e67a889e6f9e2e971be581fd0941162d1ff3093a
Tim Blankers <timblankers@gmail.com>
2016-09-13 17:21:25 +0200

    add nodejs scaffolding

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import hackathonTemplate from '../lib';
+
+describe('hackathon-template', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
