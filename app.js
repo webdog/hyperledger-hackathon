588b86d90b6fe4462697e74c697fa4c7d1562bb9
Tim Blankers <timblankers@gmail.com>
2016-09-14 14:11:14 +0200

    change to safe port

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello Hackathon!');
 });
 
-app.listen(6000, function () {
-  console.log('Boilerplate app listening on port 6000');
+app.listen(3000, function () {
+  console.log('Boilerplate app listening on port 3000');
 });
