46f6f557b94dabbdea03b7a1a2ae0be6bcedec86
Tim Blankers <timblankers@gmail.com>
2016-09-21 14:30:37 +0200

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ sudo apt-get update
 sudo apt-get install -y docker-engine
 sudo usermod -aG docker $(whoami)
 
-curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m`
+curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > docker-compose
 sudo mv docker-compose /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
